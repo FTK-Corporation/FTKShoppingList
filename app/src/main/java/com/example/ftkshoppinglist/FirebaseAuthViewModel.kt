5ca4698154b906c21d12f38ea7843a1879228dcd
PortalFan202 <anibaldonoso1@gmail.com>
2023-03-22 14:20:44 +0200

    Added basic functions to Auth ViewModel

--- app/src/main/java/com/example/ftkshoppinglist/FirebaseAuthViewModel.kt
+++ app/src/main/java/com/example/ftkshoppinglist/FirebaseAuthViewModel.kt
@@ -1,6 +1,33 @@
 package com.example.ftkshoppinglist
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FirebaseAuthViewModel: ViewModel() {
+    private var user = mutableStateOf<FirebaseUser?>(null)
+
+    fun registerUser(email: String, pwd: String) {
+        Firebase.auth.createUserWithEmailAndPassword(email, pwd)
+            .addOnSuccessListener {
+                user.value = it.user
+                Log.d("FIREBASE", "Success")
+            }.addOnFailureListener {
+                Log.e("FIREBASE", it.toString())
+            }
+    }
+
+    fun signInUser(email: String, pwd: String) {
+        Firebase.auth.signInWithEmailAndPassword(email, pwd)
+            .addOnSuccessListener {
+                user.value = it.user
+                Log.d("FIREBASE", "Log In Success")
+            }
+            .addOnFailureListener {
+                Log.e("FIREBASE", it.toString())
+            }
+    }
 }
