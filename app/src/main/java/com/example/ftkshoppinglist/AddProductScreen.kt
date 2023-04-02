021140706c3eaca01c1f436bea9dde609777be86
PortalFan202 <anibaldonoso1@gmail.com>
2023-04-02 20:23:50 +0300

    Added Labels

--- app/src/main/java/com/example/ftkshoppinglist/AddProductScreen.kt
+++ app/src/main/java/com/example/ftkshoppinglist/AddProductScreen.kt
@@ -4,9 +4,7 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -26,32 +24,37 @@ fun PickImage() {
     }
 
     Column() {
+        OutlinedTextField(
+            value = storageViewModel.idInput,
+            onValueChange = { storageViewModel.idInput = it },
+            label = { Text(text = "ID") })
+        OutlinedTextField(
+            value = storageViewModel.name,
+            onValueChange = { storageViewModel.changeName(it) },
+            label = { Text(text = "Product Name") })
+        OutlinedTextField(
+            value = storageViewModel.description,
+            onValueChange = { storageViewModel.changeDescription(it) },
+            label = { Text(text = "Product Description") })
+        OutlinedTextField(
+            value = storageViewModel.aisle,
+            onValueChange = { storageViewModel.changeAisle(it) },
+            label = { Text(text = "Product Aisle") })
+        OutlinedTextField(
+            value = storageViewModel.category,
+            onValueChange = { storageViewModel.changeCategory(it) },
+            label = { Text(text = "Category") })
+        OutlinedTextField(
+            value = storageViewModel.subCategory,
+            onValueChange = { storageViewModel.changeSubCategory(it) },
+            label = { Text(text = "Sub Category") })
         Button(onClick = { launcher.launch("image/*") }) {
             Text(text = "Select Image")
         }
 
-        OutlinedTextField(
-            value = storageViewModel.idInput,
-            onValueChange = { storageViewModel.idInput = it })
-        OutlinedTextField(
-            value = storageViewModel.name,
-            onValueChange = { storageViewModel.changeName(it) })
-        OutlinedTextField(
-            value = storageViewModel.description,
-            onValueChange = { storageViewModel.changeDescription(it) })
-        OutlinedTextField(
-            value = storageViewModel.aisle,
-            onValueChange = { storageViewModel.changeAisle(it) })
-        OutlinedTextField(
-            value = storageViewModel.category,
-            onValueChange = { storageViewModel.changeCategory(it) })
-        OutlinedTextField(
-            value = storageViewModel.subCategory,
-            onValueChange = { storageViewModel.changeSubCategory(it) })
-
-        if (storageViewModel.productExists) 
+        if (storageViewModel.productExists)
             Text(text = "Product exists!")
-        
+
         Button(onClick = { storageViewModel.addProduct() }) {
             Text(text = "Add Product")
         }
