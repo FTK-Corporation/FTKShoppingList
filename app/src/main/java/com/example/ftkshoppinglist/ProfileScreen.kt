a9090e7780b833ddade024f6d0a1783fa21c5da2
PortalFan202 <anibaldonoso1@gmail.com>
2023-04-02 17:11:10 +0300

    Optimized Imports

--- app/src/main/java/com/example/ftkshoppinglist/ProfileScreen.kt
+++ app/src/main/java/com/example/ftkshoppinglist/ProfileScreen.kt
@@ -1,9 +1,7 @@
 package com.example.ftkshoppinglist
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
