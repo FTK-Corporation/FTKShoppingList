31506583d680d1c595e1f3e9af2b689e5661e684
FrackingFrick <heta.myllymaki@gmail.com>
2023-04-12 15:30:14 +0300

    Update Filtering.kt

--- app/src/main/java/com/example/ftkshoppinglist/Filtering.kt
+++ app/src/main/java/com/example/ftkshoppinglist/Filtering.kt
@@ -1,12 +1,7 @@
 package com.example.ftkshoppinglist
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -14,5 +9,5 @@ fun FilterLogic(productsViewModel: ProductsViewModel = viewModel()) {
     LaunchedEffect(null) {
         productsViewModel.fetchProducts()
     }
-    
+
 }
